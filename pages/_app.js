3f0c4f7c257255b70a1c1cf2243eb1ad09557119
Chase Rigby <chase.rigby@utexas.edu>
2023-11-21 12:28:08 -0600

    Installed bootstrap and started migrating home page

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import "bootstrap/dist/css/bootstrap.min.css"; // Import bootstrap CSS
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,62 @@
-const Home = () => {
-  return <h1>Home Page</h1>;
-};
+// import PetCard from "../components/card";
+// import Container from 'react-bootstrap/Container';
+// import SearchBar from "@/components/searchbar";
+// import QuizBanner from "@/components/quizbanner";
 
-export default Home;
+export default function Home() {
+
+  return (
+    <div>
+      <section className="hero-wrapper">
+        <p>Every pet deserves a loving HOME.<br/>
+        Adopt a paw buddy!</p>
+        <div className="search-section">
+          <h1>Find your furry friend today!</h1>
+          {/* <SearchBar/> */}
+        </div>
+      </section>
+      <section style={{display: "flex", flexDirection: "column"}}>
+        {/* <QuizBanner/> */}
+      </section>
+      <section className="near-you">
+        <div>
+          <h2>Dogs near you</h2>
+        </div>
+        <div>
+          <h2>Cats near you</h2>
+        </div>
+      </section>
+      <section>
+        <div className="container mt-5 mb-5">
+            <h2 className="text-center mb-5">Pawou Values</h2>
+            <div className="row">
+                <div className="col-md-4 text-center mb-4">
+                    <div className="circle-container">
+                        <div className="icon">
+                        </div>
+                        <h3>Save Lives</h3>
+                        <p>Our foremost commitment is to save lives by rescuing and rehoming animals in distress, providing them with a second chance for a happy, healthy future. Every day, we work tirelessly to ensure that no animal is left behind, striving to be a beacon of hope and a lifeline for pets in need.</p>
+                    </div>
+                </div>
+                <div className="col-md-4 text-center mb-4">
+                    <div className="circle-container2">
+                        <div className="icon">
+                        </div>
+                        <h3>Promote animal well-being</h3>
+                        <p>We prioritize the well-being of every animal in our care, offering them a safe haven where they receive proper medical attention, love, and nurturing to thrive. Our dedication to promoting animal well-being extends beyond adoption; we also educate our community about responsible pet ownership to create a world where every pet lives a joyful life.</p>
+                    </div>
+                </div>
+                <div className="col-md-4 text-center mb-4">
+                    <div className="circle-container">
+                        <div className="icon">
+                        </div>
+                        <h3>Advocate Compassion</h3>
+                        <p>We passionately advocate for compassion towards all creatures, emphasizing empathy and kindness as the driving force behind our mission to bring animals and humans closer together. Through our advocacy for compassion, we strive to inspire a culture of understanding and empathy, fostering relationships between pets and their owners built on love and respect.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </section>
+    </div>
+  )
+}
